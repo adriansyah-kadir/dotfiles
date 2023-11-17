032bc02fdb2e67a06e8138eb7d21e68b8c78b8a6
adriansyah-kadir <adriansyahkadir293@gmail.com>
2023-11-17 16:00:06 +0800

    dotfiles

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -3,12 +3,10 @@ local telescope = require("telescope.builtin")
 vim.keymap.set('n', 'tf', function ()
 	telescope.find_files({
 		hidden = true,
-		opts = {
-			file_ignore_patterns = {"node_modules"}
-		}
 	})
 end)
 vim.keymap.set('n', 'tb', "<cmd>Telescope buffers<cr>")
+vim.keymap.set('n', 'tg', "<cmd>Telescope live_grep<cr>")
 vim.keymap.set('n', '<c-p>', vim.cmd.Texplore)
 vim.keymap.set('n', 'o', "o<esc>")
 vim.keymap.set('n', 'O', "O<esc>")
